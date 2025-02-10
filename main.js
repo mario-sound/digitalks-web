f9a7aab4b03db6b73f4df9dff6ec878c183f8a1c
mario-sound <mario16msound@gmail.com>
2025-02-10 19:13:05 +0100

    adding animations

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-import "../scss/main.scss";
+import "./sass/style.scss";
+console.log("El script whatsappButton.js se ha cargado correctamente.");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const whatsappBtn = document.getElementById("whatsapp-btn");
+  const footer = document.querySelector("site-footer");
+
+  function adjustWhatsAppButton() {
+    const windowHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+    const documentHeight = document.body.offsetHeight;
+    const footerOffsetTop = footer.offsetTop;
+
+    if (scrollY + windowHeight >= footerOffsetTop) {
+      whatsappBtn.style.bottom = `${windowHeight - footerOffsetTop + 20}px`;
+    } else {
+      whatsappBtn.style.bottom = "20px";
+    }
+  }
+  window.addEventListener("scroll", adjustWhatsAppButton);
+});
